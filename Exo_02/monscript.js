0faaeb295f26ca2042ea9c12bbac8129040b357a
Zekka <guillaume.kerbourch@yahoo.com>
2019-05-29 12:52:24 +0200

    ajout de nouveaux fichiers

---
+++ Exo_02/monscript.js
@@ -0,0 +1,17 @@
+var a = 13;
+if(l == 0) {
+	var b =	a/2;
+	var	c = b/3;
+	var	d = c/4;
+	var e =	d/5;
+	var	f = e/6;
+	var	g = f/7;
+	var	h = g/8;
+	var	i = h/9;
+	var	j = i/10;
+	var	k = j/11;
+	var	l = k/12;;
+}
+else {
+	console.log("Le nombre est un nombre premier");
+}
